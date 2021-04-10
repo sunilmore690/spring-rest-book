d3c8a39a3d6c807d84c4477e72a49985e88b93c5
Sunil More <sunilmore690@gmail.com>
2021-04-10 20:31:37 +0530

    pass bean name through header

---
+++ src/main/java/com/example/sunilrestdemo/constants/BeanConstant.java
@@ -0,0 +1,6 @@
+package com.example.sunilrestdemo.constants;
+
+public class BeanConstant {
+    public  static  final String restbean = "bookrestservice";
+    public  static  final String dbbean = "bookdbservice";
+}
--- src/main/java/com/example/sunilrestdemo/controller/BookController.java
+++ src/main/java/com/example/sunilrestdemo/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.example.sunilrestdemo.controller;
 
+import com.example.sunilrestdemo.constants.BeanConstant;
 import com.example.sunilrestdemo.entity.Book;
 import com.example.sunilrestdemo.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,28 +15,50 @@ import java.util.Optional;
 public class BookController {
 
     @Autowired
-    @Qualifier("bookrestservice")
-    private BookService bookService;
+    @Qualifier(BeanConstant.restbean)
+    private BookService bookRestService;
+
+    @Autowired
+    @Qualifier(BeanConstant.dbbean)
+    private BookService bookDbService;
 
     @GetMapping("")
-    public List<Book> getBooks(){
-       return this.bookService.getBooks();
+    public List<Book> getBooks(@RequestHeader("bean") String bean){
+        if(bean.equalsIgnoreCase(BeanConstant.restbean)){
+            return this.bookRestService.getBooks();
+        }
+        return this.bookDbService.getBooks();
     }
+
     @GetMapping("/{bookId}")
-    public Book getBook(@PathVariable String bookId){
-        return this.bookService.getBook(bookId);
+    public Book getBook(@PathVariable String bookId,@RequestHeader("bean") String bean){
+        if(bean.equalsIgnoreCase(BeanConstant.restbean)){
+            return this.bookRestService.getBook(bookId);
+        }
+        return this.bookDbService.getBook(bookId);
     }
     @DeleteMapping("/{bookId}")
-    public Boolean deleteBook(@PathVariable  String bookId){
-        return this.bookService.deleteBook(bookId);
+    public Boolean deleteBook(@PathVariable  String bookId,@RequestHeader("bean") String bean){
+        if(bean.equalsIgnoreCase(BeanConstant.restbean)){
+            return this.bookRestService.deleteBook(bookId);
+        }
+        return this.bookDbService.deleteBook(bookId);
+
     }
     @PostMapping("")
-    public Book saveBook(@RequestBody  Book book){
-        return this.bookService.saveBook(book);
+    public Book saveBook(@RequestBody  Book book,@RequestHeader("bean") String bean){
+        if(bean.equalsIgnoreCase(BeanConstant.restbean)){
+            return this.bookRestService.saveBook(book);
+        }
+        return this.bookDbService.saveBook(book);
     }
     @PutMapping("/{bookId}")
-    public Book updateBook(@PathVariable String bookId,@RequestBody Book book){
+    public Book updateBook(@PathVariable String bookId,@RequestBody Book book,@RequestHeader("bean") String bean){
         book.setId(bookId);
-        return this.bookService.updateBook(bookId,book);
+        if(bean.equalsIgnoreCase(BeanConstant.restbean)){
+            return this.bookRestService.updateBook(bookId,book);
+        }
+        return this.bookDbService.updateBook(bookId,book);
+
     }
 }
